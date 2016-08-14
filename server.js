b3a53a8ecfbb7da7a333b2a3f917472d394dbc46
Chad Latham <chadlatham33@gmail.com>
2016-08-13 18:08:14 -0700

    Close #3 Create basis server.js file

---
+++ server.js
@@ -0,0 +1,85 @@
+'use strict';
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config({ silent: true });
+}
+
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+
+
+// Middleware
+const ev = require('express-validation');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+// Routes
+// const posts = require('./routes/posts');
+// const topics = require('./routes/topics');
+// const users = require('./routes/users');
+// const token = require('./routes/token');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+// Use Routes
+switch (app.get('env')) {
+  case 'development':
+    app.use(morgan('dev'));
+    break;
+
+  case 'production':
+    app.use(morgan('short'));
+    break;
+
+  default:
+}
+
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(posts);
+// app.use(topics);
+// app.use(users);
+// app.use(token);
+
+// Page not found handler
+app.use((_req, res, _next) => {
+  res.sendStatus(404);
+});
+
+// Global error handler
+// eslint-disable-next-line max-params
+// eslint-disable-next-line max-params
+app.use((err, _req, res, _next) => {
+  if (err.status) {
+    return res
+      .status(err.status)
+      .set('Content-Type', 'application/json')
+      .send(err);
+  }
+
+  if (err.output && err.output.statusCode) {
+    return res
+      .status(err.output.statusCode)
+      .set('Content-Type', 'text/plain')
+      .send(err.message);
+  }
+
+  // eslint-disable-next-line no-console
+  console.error(err.stack);
+  res.sendStatus(500);
+});
+
+app.listen(port, () => {
+  if (process.env.NODE_ENV !== 'test') {
+    // eslint-disable-next-line no-console
+    console.log('Listening on port', port);
+  }
+});
+
+module.exports = app;
