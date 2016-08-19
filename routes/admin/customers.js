bb90339274eda207f6ce87d7770f99949bf21163
Matthew Benavides <matthewrbenavides@gmail.com>
2016-08-19 16:32:38 -0700

    break customer routes into admin and non-admin

--- routes/admin/customers.js
+++ routes/admin/customers.js
@@ -14,8 +14,13 @@ const { checkAdmin } = require('../modules/middleware');
 const {
   sanitizeCustomer,
   processShipAddress,
-  verifySameAddress
+  verifySameAddress,
+  getPrimaryAddress,
+  getShipAddress,
+  embedAddresses,
+  mergeAddresses,
 } = require('../modules/utils');
+const Lob = require('lob')(process.env.LOB_APIKEY);
 
 // For admins to get all customers
 // Route tested and working
@@ -37,20 +42,140 @@ router.get('/customers', checkAdmin, (req, res, next) => {
     });
 });
 
-// For customers to update their own info
+// For admins to update customers
+// Route tested and working
 router.patch('/customer/:id', checkAdmin, ev(val.patchAdmin),
   (req, res, next) => {
-    const id = req.params.id;
+  const id = req.params.id;
+  const cust = processShipAddress(req.body);
+  let isSameAddress;
+  let primaryAddress = getPrimaryAddress(cust);
+  let shipAddress = getShipAddress(cust);
 
-    // Needed for explicit null conversion from 'null'
-    if (req.body.deleted === 'null') {
-      req.body.deleted = null;
-    }
+  // Needed for explicit null conversion from 'null' in body
+  if (cust.deleted === 'null') {
+    cust.deleted = null;
+  }
 
-// WORKING HERE!!!!!!!!!!!!!!
+  knex('customers')
+    .where('id', id)
+    .first()
+    .then((existingCustomer) => {
+      if (!existingCustomer) {
+        throw boom.notFound('Invalid customer id');
+      }
 
+      // Used to populate cust from existingCustomer
+      mergeAddresses(existingCustomer, primaryAddress, shipAddress);
 
-    res.send(req.body);
+      // Explicit check for same address
+      isSameAddress = verifySameAddress(primaryAddress, shipAddress);
 
+      return Lob.verification.verify(decamelizeKeys(primaryAddress))
+        .then((lobPriRes) => {
+          // Guard clause for partial match
+          if (lobPriRes.message) {
+            const err = boom.notFound(JSON.stringify(camelizeKeys(lobPriRes)));
+            err.json = true;
+            throw err;
+          }
+
+          primaryAddress = camelizeKeys(lobPriRes.address);
+
+          // Skip evaluating shipping Address
+          if (isSameAddress) {
+            shipAddress = camelizeKeys(lobPriRes.address);
+
+            return;
+          }
+
+          return Lob.verification.verify(decamelizeKeys(shipAddress))
+            .then((lobShipRes) => {
+              // Guard clause for partial match
+              if (lobShipRes.message) {
+                const jsonLobRes = JSON.stringify(camelizeKeys(lobShipRes));
+                const err = boom.notFound(jsonLobRes);
+                err.json = true;
+                throw err;
+              }
+
+              shipAddress = camelizeKeys(lobShipRes.address);
+
+              return;
+            })
+            .catch((err) => {
+              if (err.message === 'address not found') {
+                err.message = `Shipping ${err.message}`;
+              }
+
+              throw err;
+            });
+        })
+        .catch((err) => {
+          if (err.message === 'address not found') {
+            err.message = `Primary ${err.message}`;
+          }
+
+          throw err;
+        })
+    })
+    .then(() => {
+      // Addresses verified
+      embedAddresses(cust, primaryAddress, shipAddress);
+      if (!cust.password) {
+        return;
+      }
+
+      return bcrypt.hash(cust.password, 12)
+        .then((hashedPassword) => {
+          delete cust.password;
+          cust.hashedPassword = hashedPassword;
+
+          return;
+        });
+    })
+    .then(() => {
+      // Password verified and converted to hashedPassword
+      if (!cust.userName) {
+        return;
+      }
+
+      return knex('customers')
+        .where('user_name', cust.userName)
+        .first()
+        .then((existingUserName) => {
+          if (existingUserName) {
+            throw boom.conflict('User name already exists');
+          }
+
+          return;
+        });
+    })
+    .then(() => {
+      // UserName Verified
+      cust.updatedAt = new Date();
+
+      return knex('customers')
+        .where('id', id)
+        .update(decamelizeKeys(cust), '*');
+    })
+    .then((newCusts) => {
+      res.send(sanitizeCustomer(camelizeKeys(newCusts[0])));
+    })
+    .catch((err) => {
+      // Handle lob API errors for address not found
+      const primaryAddressErr = err.message === 'Primary address not found';
+      const shipAddressErr = err.message === 'Shipping address not found';
+      if (primaryAddressErr || shipAddressErr) {
+        throw boom.notFound(err.message);
+      }
+
+      // Pass through for other error types
+      throw err;
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
+
 module.exports = router;
--- routes/customers.js
+++ routes/customers.js
@@ -10,7 +10,7 @@ const { camelizeKeys, decamelizeKeys } = require('humps');
 const bcrypt = require('bcrypt-as-promised');
 const ev = require('express-validation');
 const val = require('../validations/customers');
-const { checkAuth, checkAdmin } = require('../modules/middleware');
+const { checkAuth } = require('../modules/middleware');
 const {
   sanitizeCustomer,
   processShipAddress,
@@ -24,7 +24,7 @@ const Lob = require('lob')(process.env.LOB_APIKEY);
 
 // For customers to get their info (populate forms / pull to client on login)
 // Route tested and working
-router.get('/api/customer', checkAuth, (req, res, next) => {
+router.get('/customer', checkAuth, (req, res, next) => {
   const userId = req.token.userId;
 
   knex('customers')
@@ -42,28 +42,8 @@ router.get('/api/customer', checkAuth, (req, res, next) => {
     });
 });
 
-// For admins to get all customers
-// Route tested and working
-router.get('/api/admin/customers', checkAdmin, (req, res, next) => {
-  knex('customers')
-    .then((customers) => {
-      let custs = camelizeKeys(customers);
-      custs = custs.map((cust) => {
-        delete cust.hashedPassword;
-        delete cust.createdAt;
-        delete cust.updatedAt;
-
-        return cust;
-      });
-      res.send(custs);
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
-
 // For customers to update their own info
-router.patch('/api/customer', checkAuth, ev(val.patch), (req, res, next) => {
+router.patch('/customer', checkAuth, ev(val.patch), (req, res, next) => {
   const id = req.token.userId;
   const cust = processShipAddress(req.body);
   let isSameAddress;
@@ -191,146 +171,10 @@ router.patch('/api/customer', checkAuth, ev(val.patch), (req, res, next) => {
     });
 });
 
-// For admins to update customers
-// Route tested and working
-router.patch('/api/admin/customer/:id', checkAdmin, ev(val.patchAdmin),
-  (req, res, next) => {
-  const id = req.params.id;
-  const cust = processShipAddress(req.body);
-  let isSameAddress;
-  let primaryAddress = getPrimaryAddress(cust);
-  let shipAddress = getShipAddress(cust);
-
-  // Needed for explicit null conversion from 'null' in body
-  if (cust.deleted === 'null') {
-    cust.deleted = null;
-  }
-
-  knex('customers')
-    .where('id', id)
-    .first()
-    .then((existingCustomer) => {
-      if (!existingCustomer) {
-        throw boom.notFound('Invalid customer id');
-      }
-
-      // Used to populate cust from existingCustomer
-      mergeAddresses(existingCustomer, primaryAddress, shipAddress);
-
-      // Explicit check for same address
-      isSameAddress = verifySameAddress(primaryAddress, shipAddress);
-
-      return Lob.verification.verify(decamelizeKeys(primaryAddress))
-        .then((lobPriRes) => {
-          // Guard clause for partial match
-          if (lobPriRes.message) {
-            const err = boom.notFound(JSON.stringify(camelizeKeys(lobPriRes)));
-            err.json = true;
-            throw err;
-          }
-
-          primaryAddress = camelizeKeys(lobPriRes.address);
-
-          // Skip evaluating shipping Address
-          if (isSameAddress) {
-            shipAddress = camelizeKeys(lobPriRes.address);
-
-            return;
-          }
-
-          return Lob.verification.verify(decamelizeKeys(shipAddress))
-            .then((lobShipRes) => {
-              // Guard clause for partial match
-              if (lobShipRes.message) {
-                const jsonLobRes = JSON.stringify(camelizeKeys(lobShipRes));
-                const err = boom.notFound(jsonLobRes);
-                err.json = true;
-                throw err;
-              }
-
-              shipAddress = camelizeKeys(lobShipRes.address);
-
-              return;
-            })
-            .catch((err) => {
-              if (err.message === 'address not found') {
-                err.message = `Shipping ${err.message}`;
-              }
-
-              throw err;
-            });
-        })
-        .catch((err) => {
-          if (err.message === 'address not found') {
-            err.message = `Primary ${err.message}`;
-          }
-
-          throw err;
-        })
-    })
-    .then(() => {
-      // Addresses verified
-      embedAddresses(cust, primaryAddress, shipAddress);
-      if (!cust.password) {
-        return;
-      }
-
-      return bcrypt.hash(cust.password, 12)
-        .then((hashedPassword) => {
-          delete cust.password;
-          cust.hashedPassword = hashedPassword;
-
-          return;
-        });
-    })
-    .then(() => {
-      // Password verified and converted to hashedPassword
-      if (!cust.userName) {
-        return;
-      }
-
-      return knex('customers')
-        .where('user_name', cust.userName)
-        .first()
-        .then((existingUserName) => {
-          if (existingUserName) {
-            throw boom.conflict('User name already exists');
-          }
-
-          return;
-        });
-    })
-    .then(() => {
-      // UserName Verified
-      cust.updatedAt = new Date();
-
-      return knex('customers')
-        .where('id', id)
-        .update(decamelizeKeys(cust), '*');
-    })
-    .then((newCusts) => {
-      res.send(sanitizeCustomer(camelizeKeys(newCusts[0])));
-    })
-    .catch((err) => {
-      // Handle lob API errors for address not found
-      const primaryAddressErr = err.message === 'Primary address not found';
-      const shipAddressErr = err.message === 'Shipping address not found';
-      if (primaryAddressErr || shipAddressErr) {
-        throw boom.notFound(err.message);
-      }
-
-      // Pass through for other error types
-      throw err;
-    })
-    .catch((err) => {
-      next(err);
-    });
-});
-
 // Customers: Create Customer (with or without proper registration security).
 // req.body: firstName, lastName, [phone], email, [userName], [password], addressLine1, [addressLine2], addressCity, addressState, addressZip, addressCountry, shipFirstName, shipLastName, shipAddressLine1, [shipAddressLine2], shipAddressCity, shipAddressState, shipAddressZip, shipAddressCountry
 // Route tested and working
-router.post('/api/customers', ev(val.post), (req, res, next) => {
+router.post('/customers', ev(val.post), (req, res, next) => {
   const cust = processShipAddress(req.body);
   const isSameAddress = verifySameAddress(cust);
   let primaryAddress = getPrimaryAddress(cust);
@@ -437,7 +281,7 @@ router.post('/api/customers', ev(val.post), (req, res, next) => {
 
 // For customers to delete themselves
 // Route tested and working
-router.delete('/api/customer', checkAuth, (req, res, next) => {
+router.delete('/customer', checkAuth, (req, res, next) => {
   const id = req.token.userId;
 
   knex('customers')
