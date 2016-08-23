c4fb209d82c1c4d22e4171ce9c8418d6eb0fd14c
Matthew Benavides <matthewrbenavides@gmail.com>
2016-08-22 22:25:15 -0700

    Starting to build out home/product pages

--- app/components/ProductPage.jsx
+++ app/components/ProductPage.jsx
@@ -6,7 +6,6 @@ const ProductPage = React.createClass({
   },
 
   render() {
-    console.log('render');
     if (this.props.products.length === 0) {
       return <div></div>;
     }
@@ -15,10 +14,24 @@ const ProductPage = React.createClass({
     const product = this.props.products.filter((prod) => {
       return prod.id === id;
     })[0];
-    return <div>
 
-      <img src={product.images[0].imageUrl} />
-      <h1>This is the Product Page</h1>
+    console.log(product);
+    return <div className="row product-space container">
+      <div className="col s5">
+        <img className="col s12"src={product.images[0].imageUrl} />
+      </div>
+
+      <div className="col s7">
+        <div className="col s10 offset-1 product-info-space">
+          <div className="col s12 product-info-inner">
+            <h1>{product.name}</h1>
+            <h5 className="col s6">${product.price}</h5>
+            <p className="col s4 question">Ask a question</p>
+            <h5>dimensions: {product.dimensions}</h5>
+          </div>
+        </div>
+      </div>
+
     </div>;
   }
 });
