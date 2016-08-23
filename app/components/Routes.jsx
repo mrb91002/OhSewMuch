45d1e794ed56f9952af2d88bddfb9ab33ebec151
Matthew Benavides <matthewrbenavides@gmail.com>
2016-08-23 16:07:48 -0700

    Added cart route

--- app/components/Routes.jsx
+++ app/components/Routes.jsx
@@ -11,6 +11,7 @@ import React from 'react';
 import ProductPage from 'components/ProductPage';
 import LoginPage from 'components/LoginPage';
 import RegisterPage from 'components/RegisterPage';
+import Cart from 'components/Cart';
 
 const Routes = React.createClass({
   render() {
@@ -20,7 +21,7 @@ const Routes = React.createClass({
         <Route component={ProductPage} path="product/:id" />
         <Route component={LoginPage} path="login" />
         <Route component={RegisterPage} path="register" />
-
+        <Route component={Cart} path="cart" />
         {/* The 404 error handler */}
         <Route component={NotFound} path="*" />
       </Route>
