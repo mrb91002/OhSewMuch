2f5926eb439d42c0499b03c85651636d0dd6c7fe
Chad Latham <chadlatham33@gmail.com>
2016-08-24 09:06:34 -0700

    Adjust CSS

--- app/components/PaymentPage.jsx
+++ app/components/PaymentPage.jsx
@@ -25,6 +25,100 @@ const PaymentPage = React.createClass({
   },
 
   render() {
+    // Sandbox AppID
+    // const applicationId = 'sandbox-sq0idp-JQWJQ16-z103Dcor0PDo4Q';
+    //
+    // const paymentForm = new SqPaymentForm({
+    //   applicationId: applicationId,
+    //   inputClass: 'sq-input',
+    //   inputStyles: [
+    //     {
+    //       fontSize: '15px'
+    //     }
+    //   ],
+    //   cardNumber: {
+    //     elementId: 'sq-card-number',
+    //     placeholder: '•••• •••• •••• ••••'
+    //   },
+    //   cvv: {
+    //     elementId: 'sq-cvv',
+    //     placeholder: 'CVV'
+    //   },
+    //   expirationDate: {
+    //     elementId: 'sq-expiration-date',
+    //     placeholder: 'MM/YY'
+    //   },
+    //   postalCode: {
+    //     elementId: 'sq-postal-code'
+    //   },
+    //   callbacks: {
+    //
+    //     // Called when the SqPaymentForm completes a request to generate a card
+    //     // nonce, even if the request failed because of an error.
+    //     cardNonceResponseReceived: function(errors, nonce, cardData) {
+    //       if (errors) {
+    //         console.log("Encountered errors:");
+    //
+    //         // This logs all errors encountered during nonce generation to the
+    //         // Javascript console.
+    //         errors.forEach(function(error) {
+    //           console.log('  ' + error.message);
+    //         });
+    //
+    //       // No errors occurred. Extract the card nonce.
+    //       } else {
+    //
+    //         // Delete this line and uncomment the lines below when you're ready
+    //         // to start submitting nonces to your server.
+    //         alert('Nonce received: ' + nonce);
+    //
+    //
+    //         /*
+    //           These lines assign the generated card nonce to a hidden input
+    //           field, then submit that field to your server.
+    //           Uncomment them when you're ready to test out submitting nonces.
+    //
+    //           You'll also need to set the action attribute of the form element
+    //           at the bottom of this sample, to correspond to the URL you want to
+    //           submit the nonce to.
+    //         */
+    //         // document.getElementById('card-nonce').value = nonce;
+    //         // document.getElementById('nonce-form').submit();
+    //
+    //       }
+    //     },
+    //
+    //     unsupportedBrowserDetected: function() {
+    //       // Fill in this callback to alert buyers when their browser is not supported.
+    //     },
+    //
+    //     // Fill in these cases to respond to various events that can occur while a
+    //     // buyer is using the payment form.
+    //     inputEventReceived: function(inputEvent) {
+    //       switch (inputEvent.eventType) {
+    //         case 'focusClassAdded':
+    //           // Handle as desired
+    //           break;
+    //         case 'focusClassRemoved':
+    //           // Handle as desired
+    //           break;
+    //         case 'errorClassAdded':
+    //           // Handle as desired
+    //           break;
+    //         case 'errorClassRemoved':
+    //           // Handle as desired
+    //           break;
+    //         case 'cardBrandChanged':
+    //           // Handle as desired
+    //           break;
+    //         case 'postalCodeChanged':
+    //           // Handle as desired
+    //           break;
+    //       }
+    //     }
+    //   }
+    // });
+
     return <div className="container">
       <div className="row payment-form">
         <Paper
